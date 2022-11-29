41a281b5d4947edbe58e41e8412e51993aad85cb
Levan Kopaliani <ciceron23x@gmail.com>
2022-11-29 19:34:55 +0400

    routes + navbar

---
+++ src/components/SidebarData.js
@@ -0,0 +1,10 @@
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+function SidebarData() {
+  return <div>SidebarData</div>;
+}
+
+export default SidebarData;
